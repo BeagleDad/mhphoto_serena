1b9f5f99526d7fa2141ab6b19fe4b7c7847c315a
Matt Hoffman <formatt.hoffman@gmail.com>
2022-01-01 13:03:11 -0700

    Updated files from previous, broken, version to this latest, working, version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,60 @@
 module.exports = {
   siteMetadata: {
-    title: `Serena`,
-    description: `Photography Theme`,
-    author: "Gatsby",
-    image: "https://gatsby-theme-serena.netlify.com/og-image.webp",
-    name: `Serena`,
-    tagline: `Photography Theme`,
+    title: `Matt Hoffman Photography`,
+    description: `Beautiful, professional photographs for marketing real estate. Serving Prescott Valley, Prescott, and surrounding area.`,
+    author: "Matt Hoffman",
+    image: "https://www.matthoffmanphoto.com/static/de9cd208614cdc2803b70c0cee91982f/e7136/LorraineCt_2491.webp",
+    name: `Matt Hoffman Photography`,
+    tagline: `Beautiful Images for Real Estate`,
+    menuLinks: [
+      {
+        name: "Photo Gallery",
+        url: `/`,
+        type: "internal"
+      },
+      {
+        name: "Drone Services",
+        url: `/drone/`,
+        type: "internal"
+      },
+      // {
+      //   name: "Pricing",
+      //   url: "/prices/",
+      //   type: "internal" // internal or anchor
+      // },
+      {
+        name: "Home Prep",
+        url: "/home-prep/",
+        type: "internal"
+      },
+      {
+        name: "Terms of Service",
+        url: "/terms",
+        type: "internal"
+      },
+      {
+        name: "About",
+        url: "/about",
+        type: "internal"
+      },
+      {
+        name: "Contact",
+        url: "/contact-me/",
+        type: "internal"
+      },
+    ],
     social: [
       {
         name: `Facebook`,
-        url: `https://facebook.com`,
+        url: `https://facebook.com/MattHoffmanPhoto`,
       },
       {
         name: `Twitter`,
-        url: `https://twitter.com`,
+        url: `https://twitter.com/socialmatt7`,
       },
       {
         name: `Instagram`,
-        url: `https://instagram.com`,
+        url: `https://instagram.com/socialmatt7`,
       },
     ],
   },
@@ -25,7 +62,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Serena`,
+        name: `Serena (n)`,
         short_name: `Serena`,
         start_url: `/`,
         background_color: `#fff`,
@@ -36,7 +73,9 @@ module.exports = {
     },
     {
       resolve: `gatsby-theme-serena`,
-      options: {},
+      options: {
+        margin: `1`
+      },
     },
   ],
 }
